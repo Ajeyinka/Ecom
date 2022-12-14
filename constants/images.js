56b4ae32a6988edb529c28ad7e2a1cb0f165bada
Olayinka <ajeyinka12@gmail.com>
2022-12-14 19:43:34 +0100

    First commit

---
+++ constants/images.js
@@ -0,0 +1,12 @@
+import bag from 'asset/bag.png';
+import coat from 'asset/coat.png';
+import heel from 'asset/heel.png';
+import jacket from 'asset/jacket.png';
+
+
+export default {
+    bag,
+    coat,
+    heel,
+    jacket
+};
